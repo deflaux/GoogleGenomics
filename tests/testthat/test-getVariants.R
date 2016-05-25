2c1c0ca5eb4f8576c668a3cde3bd395afe234dbd
Nicole Deflaux <deflaux@google.com>
2016-05-25 16:40:12 -0700

    Add tests for multi-allelic data.

--- tests/testthat/test-getVariants.R
+++ tests/testthat/test-getVariants.R
@@ -5,6 +5,11 @@ fetchVariants <- function(...) {
               start=50300077, end=50301500, ...)
 }
 
+fetchMultiAllelicVariants  <- function(...) {
+  getVariants(datasetId="3049512673186936334", chromosome="chr17",
+              start=41196820, end=41196821, ...)
+}
+
 test_that("Raw variants are fetched correctly", {
   # Get raw variants from Variants API
   variants <- fetchVariants()
@@ -29,6 +34,36 @@ test_that("Variants are converted to VRanges correctly", {
   # Get VRanges from the Variants API
   vranges <- fetchVariants(converter=variantsToVRanges)
   expect_equal(length(vranges), 27)
+  expect_equal(mode(vranges), "S4")
+  expect_equal(class(vranges)[1], "VRanges")
+})
+
+test_that("Multi-allelic variants are converted to GRanges correctly", {
+  # Get GRanges from the Variants API
+  #  granges <- fetchMultiAllelicVariants(converter=variantsToGRanges)
+
+  variants <- fetchMultiAllelicVariants()
+  # Remove non-variant segments
+  only_variants <- Filter(function(v) { 1 <= length(v$alternateBases)}, variants)
+  # Remove multi-allelic variants
+  biallelic_variants <- lapply(only_variants, function(v) {
+    if(1 < length(v$alternateBases)) {
+      v$alternateBases = v$alternateBases[[1]]
+    }
+    v
+  })
+
+  granges <- variantsToGRanges(biallelic_variants)
+
+  expect_equal(length(granges), 2)
+  expect_equal(mode(granges), "S4")
+  expect_equal(class(granges)[1], "GRanges")
+})
+
+test_that("Multi-allelic variants are converted to VRanges correctly", {
+  # Get VRanges from the Variants API
+  vranges <- fetchMultiAllelicVariants(converter=variantsToVRanges)
+  expect_equal(length(vranges), 17)
   expect_equal(mode(vranges), "S4")
   expect_equal(class(vranges)[1], "VRanges")
 })
